fcb286d389de5f0799b1861f86328f51ce5d3b1b
Akiko Takano <akiko.pusu@gmail.com>
2019-03-05 10:47:18 +0900

    Add iterator sample.

--- sample.java
+++ sample.java
@@ -20,5 +20,18 @@ public class Main {
       }
       
       System.out.println("Hello world!");
+      
+      List<Integer> list = new LinkedList<>(Arrays.asList(0, 3, 1, 4, 1, 5, 9, 2, 6));
+
+      Iterator<Integer> iterator = list.iterator();
+      while (iterator.hasNext()) {
+        int num = iterator.next();
+        if (num == 0) {
+          // 0 は削除
+          iterator.remove();
+        }
+      }
+
+      System.out.println(list); // -> [3, 1, 4, 1, 5, 9, 2, 6]
     }
 }
